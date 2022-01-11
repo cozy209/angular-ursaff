161eed23bc7a5c9945df832e73e421185629a5c4
Virginie <virginie.snsn@gmail.com>
2022-01-11 14:09:10 +0100

    Revert "init ngxs"
    This reverts commit 7f038ef408a7a91858f19df31c223c9e15bd5cbd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
-import { NgxsModule } from '@ngxs/store';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
-    NgxsModule.forRoot([null], {
-      developmentMode: !environment.production
-    }),
     BrowserModule
   ],
   providers: [],
